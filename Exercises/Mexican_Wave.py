da1bfb97b77f883d465daa0188a033bcfdbc07e7
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-06-27 17:57:47 +0200

    add Mexican_wave exercise

---
+++ Exercises/Mexican_Wave.py
@@ -0,0 +1,11 @@
+def wave(people):
+    """
+
+    :param people: a word or words to be waved
+    :return: a list of words with capital letter moving through
+    """
+    return [people[:i] + people[i].upper() + people[i + 1:] for i in range(len(people)) if people[i].isalpha()]
+
+# isalpha() - check if all the characters in the text are letters and not: " ", $, %, #, etc.
+
+print(wave('aloe vera'))
