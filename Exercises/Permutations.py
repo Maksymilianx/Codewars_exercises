d3bb2e4d2de6a9983cdae60b22a46082f85726ce
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-06-30 21:59:35 +0200

    add Permutations exercise

---
+++ Exercises/Permutations.py
@@ -0,0 +1,9 @@
+def permutations(string):
+    result = {string}
+    if len(string) == 2:
+        result.add(string[1] + string[0])
+    elif len(string) > 2:
+        for i, c in enumerate(string):
+            for x in string[:1] + string[i + 1:]:
+                result.add(c + x)
+    return list(result)
