54da96748946a9c5d4e03e866a9e59ed195fb75f
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-07-02 14:22:26 +0200

    add Sum_of_pairs exercise

---
+++ Exercises/Sum_of_pairs.py
@@ -0,0 +1,17 @@
+def sum_pairs(ints, s):
+    """
+
+    :param ints: any integers
+    :param s: number to find
+    :return: two integers that in a sum gives param s
+    """
+    cache = set()
+    # set() method is used to convert any of the iterable to sequence of iterable elements.
+    # Returns empty if no elements is passed.
+    for i in ints:
+        if s - i in cache:
+            return [s - i, i]
+        cache.add(i)
+
+# print(sum_pairs([1,2,3,4,5,6], 5))
+
