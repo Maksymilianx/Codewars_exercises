7b0ed45090f203f9e35a298d597d5176082a819f
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-07-12 12:07:04 +0200

    add Next_bigger_number_with_the_same_digits exercise

---
+++ Exercises/Next_bigger_number_with_the_same_digits.py
@@ -0,0 +1,20 @@
+def next_bigger(n):
+    """
+
+    :param n: an integer
+    :return: a next bigger integer
+    """
+    arr = list(str(n))
+    # max_n reverse and sort the list to get the largest number
+    max_n = int("".join(sorted(arr, reverse=True)))
+    # min_n sort a list to get the min. number
+    min_n = sorted(arr)
+    m = n
+    while m <= max_n:
+        m += 1
+        # if number is found in our min_n list then return the new number
+        if sorted(list(str(m))) == min_n:
+            return m
+    return -1
+
+# print(next_bigger(12))
