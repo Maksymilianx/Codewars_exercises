ca8c8e54eadaca50499220749cee0efc833abc99
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-07-23 08:38:33 +0200

    add Sum_of_pairs tests

---
+++ Exercises/Sum_of_pairs_test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from Sum_of_pairs import *
+
+
+class Test(TestCase):
+    def test_volume_case1(self):
+        self.assertListEqual(sum_pairs([1, 2, 3, 4, 5, 6], 6), [2, 4])
+        self.assertListEqual(sum_pairs([1, 2, 3, 4, 5, 6], 4), [1, 3])
+        self.assertListEqual(sum_pairs([1, 2, 3, 4, 5, 6], 5), [2, 3])
+        self.assertListEqual(sum_pairs([1, 2, 3, 4, 5, 6,7,8,9], 7), [3, 4])
