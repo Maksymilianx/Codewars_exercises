c1ace82b1385c06dd661fab82a0da1ae1a223ae3
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-07-28 13:46:04 +0200

    add Find_Short_Words exercise

--- Exercises/Find_Short_Words.py
+++ Exercises/Find_Short_Words.py
@@ -1,4 +1,9 @@
 def find_short_words(fsw):
+    """
+
+    :param fsw: a list of strings
+    :return: a list of words smaller than 5
+    """
     lst = []
     for i in fsw:
         if len(i) < 5:
