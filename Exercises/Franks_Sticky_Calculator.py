574453d661e8ea3c851edc41b3626c44557a3a7d
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-04-03 11:08:23 +0200

    add quicker implementation with eval method

--- Exercises/Franks_Sticky_Calculator.py
+++ Exercises/Franks_Sticky_Calculator.py
@@ -8,3 +8,7 @@ def sticky_calc(operation, val1, val2):
         return round(stick * val2)
     elif operation == '/':
         return round(stick / val2)
+
+
+def sticky_calc2(op, val1, val2):
+    return eval(f"{round(val1)}{round(val2)} {op} {round(val2)}")
